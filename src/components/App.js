bfd43e8f5271555325e05d2ec8cd944ea17df86f
nachocarher <nachoch64@correo.ugr.es>
2025-04-30 14:03:24 +0200

    Refactor App component to integrate local storage for task management

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
-import { useState, useRef, useReducer } from "react";
+import { useState, useEffect, useRef, useReducer } from "react";
 import logo from "../assets/logo.avif";
 import TasksList from "./TasksList";
-
-// a mejorar: añadir el local storage
 
 function reducer(state, action) {
   switch (action.type) {
     case "add":
       if (!action.payload.task) return state;
-
       return [...state, action.payload];
     case "delete":
       const newList = state.filter((task) => task.id !== action.payload.id);
@@ -20,25 +17,25 @@ function reducer(state, action) {
 
 function App() {
   const initialValue = { task: "", id: -1 };
-  const [state, dispatch] = useReducer(reducer, []);
   const [taskToAdd, setTaskToAdd] = useState(initialValue);
   const counterRef = useRef(0);
+  const [state, dispatch] = useReducer(reducer, [], function () {
+    const storedValue = localStorage?.getItem("tasks")?.split(",");
+    if (storedValue?.[0] === "") {
+      return [];
+    }
+    return storedValue ? JSON.parse(storedValue) : [];
+  });
 
-  // const [tasksList, setTasksList] = useState(function () {
-  //   const storedValue = localStorage.getItem("tasks").split(",");
-  //   return storedValue ? storedValue : [];
-  // });
-
-  // useEffect(
-  //   function () {
-  //     localStorage.setItem("tasks", tasksList);
-  //   },
-  //   [tasksList]
-  // );
+  useEffect(
+    function () {
+      localStorage.setItem("tasks", JSON.stringify(state));
+    },
+    [state]
+  );
 
   function handleSubmit(event) {
     event.preventDefault();
-    // setTasksList([...tasksList, taskToAdd]);
     dispatch({ type: "add", payload: taskToAdd });
     counterRef.current++;
     setTaskToAdd(initialValue);
